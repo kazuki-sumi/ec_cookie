81828c2619521046167b0a7a82e27fa70213f9dd
鷲見和希 <cs5200@icloud.com>
2019-10-08 14:24:11 +0900

    devise適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root "product_categories#index"
   devise_for :users
   resources :product_categories do
     resources :products, only: [:index]
   end
   resources :products, except: [:index]
-  resources :users
+  resources :users, only: [:show]
   resources :carts
 end
