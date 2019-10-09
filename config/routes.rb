47fc21976d9885a2e5c08a893208d7965d9ac4c8
鷲見和希 <cs5200@icloud.com>
2019-10-09 14:48:43 +0900

    ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "product_categories#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   resources :product_categories do
     resources :products, only: [:index]
   end
