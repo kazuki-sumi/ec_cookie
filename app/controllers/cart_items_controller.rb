471f33493b09986ce1b0e10f5f033b10c6d9c51c
鷲見和希 <cs5200@icloud.com>
2019-10-24 17:16:21 +0900

    修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class CartItemsController < ApplicationController
 
   def create
+    binding.pry
     raise "この商品は存在しません" unless product = Product.find_by(id: params[:product_id])
     render product_path(product) unless @cart = Cart.find_by(id: params[:cart_id])
     @item = @cart.cart_items.create!(product_id: params[:product_id], product_count: params[:product_count])
@@ -8,11 +9,14 @@ class CartItemsController < ApplicationController
   end
 
   def update
-    raise "この商品は存在しません" unless product = Product.find_by(id: params[:product_id])
-    render product_path(product) unless @cart = Cart.find_by(id: params[:id])
+    return "この商品は存在しません" unless product = Product.find_by(id: params[:cart_item][:product_id])
+    render product_path(product) unless @cart = Cart.find_by(id: params[:cart_id])
     @item = CartItem.find_by(id: params[:id])
-    @item ? @item.update!(product_count: params[:product_count]) : return
-    binding.pry
-    redirect_to carts_path
+    if @item
+      @item.update!(product_count: params[:cart_item][:product_count])
+      redirect_to carts_path
+    else
+      render "carts/index"
+    end
   end
 end
