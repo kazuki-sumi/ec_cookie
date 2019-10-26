784502f76fddea3a0d5c76d53530b738fe927861
鷲見和希 <cs5200@icloud.com>
2019-10-26 15:10:04 +0900

    ログインのflash修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,8 +14,9 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
+    # set_flash_message!(:notice, :signed_in) 応急処置
     sign_in(resource_name, resource)
+    flash[:success] = t("users.session.signed_in")
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
   end
