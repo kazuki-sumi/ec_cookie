d125b99ac9a78cfd707876863dbbf63c82b4cf89
鷲見和希 <cs5200@icloud.com>
2020-03-17 00:50:19 +0900

    カテゴリー登録機能作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,28 @@ class CategoriesController < ApplicationController
     @products = @categories.map { |category| category.products.where("contain > 1").order(contain: :asc).first }
     @single_price = @categories.map { |category| category.products.where(contain: 1) }
   end
+
+  def new
+    @category = Category.new
+    categories = Category.all
+    @large_categories = categories.map { |category| [category.name, category.id] if category.path.nil? }
+    @medium_categories = categories.map { |category| [category.name, "#{category.path}/#{category.id}"] unless category.path&.include?("/") || category.path.nil? }
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "成功"
+      redirect_to new_category_path
+    else
+      flash[:alert] = "失敗"
+      render :new
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name, :category_summary, :path)
+    end
 end
