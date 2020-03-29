8d1573961e2b3b09bf4e8d6e1513a2f22689ad6e
鷲見和希 <cs5200@icloud.com>
2020-03-29 23:36:08 +0900

    商品登録作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,10 +15,20 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.where(path: nil).map { |category| [category.name, category.id] }
+    @product_category = @product.product_categories.build
   end
 
   def create
     @product = Product.new(product_params)
+    if @product.save
+      @product.product_categories.create(category_id: params[:product][:category])
+      flash[:success] = "成功"
+      redirect_to new_product_path
+    else
+      flash[:alert] = "失敗"
+      render :new
+    end
   end
 
   private
@@ -28,6 +38,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:category_id, :name, :contain, :price)
+      params.require(:product).permit(:size, :name, :product_description, :price, :product_number)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :product_categories
+  validates :name, presence: true, length: { maximum: 191 }
+  validates :category_summary, length: { maximum: 65535 }
 end
